ae1632d5ea24dc4be182b522c034c70c02e39504
Julia <lorrayne@usf.edu>
2025-02-16 11:27:37 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -9,3 +9,22 @@ function calculateHourlyWage(salary, hoursPerWeek) {
     let hourlyWage = salary / (hoursPerWeek * 52)
     return console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`) // logging result to the console
 }
+
+//Task 3: Arrow Function
+const calculateLoyaltyDiscount = (amount,years) => {
+    let discount = 0
+    if (years >=5) {
+        discount = amount * 0.15
+    }
+    else if (years >=3 && years <5) {
+        discount = amount * 0.1
+    }
+    else {
+        discount = amount * 0.05
+    }
+    let discountedPrice = amount - discount  //calculates the discounted price
+    return console.log(`Discounted Price: $${discountedPrice.toFixed(2)}`)
+}; //arrow function calculateLoyaltyDiscount(amount, years) with the following conditions:
+//years >= 5: 15% discount
+//years >= 3: 10% discount
+//years < 3: 5% discount
